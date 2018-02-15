732e8c34662e430d77432a30111250dde5fbfdf4
Suranart Niamcome <suranartnc@gmail.com>
2018-02-15 11:57:24 +0700

    Install next-routes

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,20 @@
 import React from 'react'
-import Link from 'next/link'
+import { Link } from '../routes'
 
 export default function HomePage() {
   return (
     <div>
       <p>Home Page</p>
-      <Link href="/about">
-        <a>Go to About page</a>
-      </Link>
+      <p>
+        <Link route="about">
+          <a>Go to About page</a>
+        </Link>
+      </p>
+      <p>
+        <Link route="entry" params={{ id: 1234 }}>
+          <a>Go to Entry: 1234</a>
+        </Link>
+      </p>
     </div>
   )
 }
---
+++ routes.js
@@ -0,0 +1,12 @@
+const nextRoutes = require('next-routes')
+const routes = (module.exports = nextRoutes())
+
+// API
+// routes.add(route_name, pattern = /name, page_filename = name)
+
+// Example
+// routes.add('article', '/article/:id(\\d+)')
+
+routes.add('home', '/', 'index')
+routes.add('about', '/about/')
+routes.add('entry', '/:id(\\d+)/')
