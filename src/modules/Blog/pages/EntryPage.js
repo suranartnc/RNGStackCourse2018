e27b48c06ac8df7334f60fdc8a3f61ca48e551c5
Suranart Niamcome <suranartnc@gmail.com>
2018-05-26 11:06:32 +0700

    Provide solution

--- src/modules/Blog/pages/EntryPage.js
+++ src/modules/Blog/pages/EntryPage.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { EntryList } from './HomePage'
+
+const api = 'http://localhost:4000/posts'
 
 const styles = {
   article: {
@@ -9,13 +12,56 @@ const styles = {
   }
 }
 
-export default function EntryPage(props) {
+function EntryContent({ entry }) {
   return (
     <article style={styles.article}>
-      <h1>Title of Entry ID: {props.match.params.id}</h1>
-      <p style={styles.body}>
-        This is body of Entry ID: {props.match.params.id}
-      </p>
+      <h1>{entry.title}</h1>
+      <p style={styles.body} dangerouslySetInnerHTML={{ __html: entry.body }} />
     </article>
   )
+}
+
+export default class EntryPage extends React.Component {
+  state = {
+    entry: {},
+    entries: []
+  }
+
+  getEntry(id) {
+    fetch(api + '/' + id)
+      .then(res => res.json())
+      .then(entry => {
+        this.setState({
+          entry
+        })
+      })
+  }
+
+  componentDidMount() {
+    this.getEntry(this.props.match.params.id)
+
+    fetch(api)
+      .then(res => res.json())
+      .then(entries => {
+        this.setState({
+          entries
+        })
+      })
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const id = this.props.match.params.id
+    if (id !== prevProps.match.params.id) {
+      this.getEntry(id)
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <EntryContent entry={this.state.entry} />
+        <EntryList entries={this.state.entries} />
+      </div>
+    )
+  }
 }
--- src/modules/Blog/pages/HomePage.js
+++ src/modules/Blog/pages/HomePage.js
@@ -1,12 +1,42 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function HomePage() {
+const api = 'http://localhost:4000/posts'
+
+export function EntryList({ entries }) {
   return (
-    <div>
-      <p>This is homepage.</p>
-      <p>&nbsp;</p>
-      <Link to="/entry/1234">Link to Entry ID: 1234</Link>
-    </div>
+    <ul>
+      {entries.map(function(entry) {
+        return (
+          <li key={entry.id}>
+            <Link to={`/entry/${entry.id}`}>{entry.title}</Link>
+          </li>
+        )
+      })}
+    </ul>
   )
+}
+
+export default class HomePage extends React.Component {
+  state = {
+    entries: []
+  }
+
+  componentDidMount() {
+    fetch(api)
+      .then(res => res.json())
+      .then(entries => {
+        this.setState({
+          entries
+        })
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <EntryList entries={this.state.entries} />
+      </div>
+    )
+  }
 }
