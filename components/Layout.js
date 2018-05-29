27ab5bf98d5b2d6d790966d4f5f2edcf18acd508
SuranartN <suranartnc@gmail.com>
2018-05-29 22:30:37 +0700

    Add styled-components

--- components/Layout.js
+++ components/Layout.js
@@ -1,69 +1,76 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled, { injectGlobal } from 'styled-components'
 
 import { Link } from '@router'
 import LoadingIndicator from './LoadingIndicator'
 
+injectGlobal`
+  body {
+    font-size: 16px;
+  }
+  a {
+    color: black;
+    text-decoration: none;
+  }
+`
+
+const Wrapper = styled.div`
+  max-width: 1024px;
+  margin: 0 auto;
+`
+
+const Logo = styled.div`
+  padding: 20px 15px;
+  text-align: center;
+  font-size: 24px;
+  font-weight: bold;
+
+  a {
+    color: #666;
+    text-decoration: none;
+  }
+`
+
+const Nav = styled.nav`
+  display: flex;
+  border-top: 1px solid #eee;
+  border-bottom: 1px solid #eee;
+
+  a {
+    padding: 10px 15px;
+    color: #666;
+    text-decoration: none;
+  }
+`
+
+const Main = styled.main`
+  margin-top: 50px;
+  padding: 0px 15px;
+`
+
 export default function Layout({ children }) {
   return (
-    <div className="wrapper">
+    <Wrapper>
       <LoadingIndicator />
 
-      <div className="logo">
+      <Logo>
         <Link route="home">
           <a>My Blog App</a>
         </Link>
-      </div>
+      </Logo>
 
-      <nav>
+      <Nav>
         <Link route="home">
           <a>Home</a>
         </Link>
         <Link route="about">
           <a>About</a>
         </Link>
-      </nav>
+      </Nav>
 
-      <main>{children}</main>
-
-      <style jsx global>{`
-        body { 
-          margin: 0;
-          font-family: Arial, 'sans-serif';
-        }
-      `}</style>
-
-      <style jsx>{`
-        .wrapper {
-          max-width: 1024px;
-          margin: 0 auto;
-        }
-        .logo {
-          padding: 20px 15px;
-          text-align: center;
-          font-size: 24px;
-          font-weight: bold;
-        }
-        .logo a {
-          color: #666;
-          text-decoration: none;
-        }
-        nav {
-          display: flex;
-          border-top: 1px solid #eee;
-          border-bottom: 1px solid #eee;
-        }
-        nav a {
-          padding: 10px 15px;
-          color: #666;
-          text-decoration: none;
-        }
-        main {
-          margin-top: 50px;
-          padding: 0px 15px;
-        }
-      `}</style>
-    </div>
+      <Main>{children}</Main>
+    </Wrapper>
   )
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import Document, { Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet()
+    const page = renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />)
+    )
+    const styleTags = sheet.getStyleElement()
+    return { ...page, styleTags }
+  }
+
   render() {
-    const { buildManifest: { css } } = this.props
+    const {
+      buildManifest: { css }
+    } = this.props
 
     return (
       <html>
@@ -15,6 +27,7 @@ export default class MyDocument extends Document {
           {css.map(file => {
             return <link rel="stylesheet" href={`/_next/${file}`} key={file} />
           })}
+          {this.props.styleTags}
         </Head>
         <body>
           <Main />
