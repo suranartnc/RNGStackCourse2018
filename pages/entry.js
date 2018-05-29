5aab64f702ff16c5c9b7c5a406f6a1e7a83f0135
SuranartN <suranartnc@gmail.com>
2018-05-29 19:48:08 +0700

    Use grid from ant design

--- pages/entry.js
+++ pages/entry.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 import Head from 'next/head'
+import { Row, Col } from 'antd'
 
 function EntryContent({ entry }) {
   return (
@@ -16,7 +17,9 @@ function EntryContent({ entry }) {
 
 class EntryPage extends React.Component {
   static async getInitialProps(ctx) {
-    const response = await axios.get(`http://localhost:4000/posts/${ctx.query.id}`)
+    const response = await axios.get(
+      `http://localhost:4000/posts/${ctx.query.id}`
+    )
 
     return {
       entry: response.data
@@ -26,11 +29,17 @@ class EntryPage extends React.Component {
   render() {
     return (
       <div>
-        <EntryContent entry={this.props.entry} />
+        <Row>
+          <Col xs={24} lg={16}>
+            <EntryContent entry={this.props.entry} />
+          </Col>
+          <Col xs={24} lg={8}>
+            Sidebar
+          </Col>
+        </Row>
       </div>
     )
   }
 }
-
 
 export default EntryPage
